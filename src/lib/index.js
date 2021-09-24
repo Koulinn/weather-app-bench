103a02fb06a1c2680e8a538ccf71f9b97c48fca8
Koulinn <drdverzola@gmail.com>
2021-09-24 11:37:32 +0100

    created a Get request function

---
+++ src/lib/index.js
@@ -0,0 +1,28 @@
+export const getCityCurrentWeather = async (city)=>{
+    try {
+        const URL = process.env.REACT_APP_URL_CURRENT_WEATHER + `?q=${city}&appid=` + process.env.REACT_APP_API_KEY
+        const res = await fetch(URL)
+        if(res.ok){
+            const serverData = await res.json()
+            const {coord} = serverData
+            return {
+                coord,
+                serverData
+            }
+
+        } else {
+            // Request Error handling
+            console.log(res)
+        }
+
+    } catch (error) {
+        //Server request error handling
+        console.log(error)
+    }
+}
+
+const requests ={
+    getCurrentWeather: getCityCurrentWeather
+}
+
+export default requests
